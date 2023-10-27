5cff84cbcad0e37ff8a4c03ed422f7d36a5ed5cf
JoshChiaCPP <143152464+JoshChiaCPP@users.noreply.github.com>
2023-10-26 22:40:21 -0700

    Update employee.h

--- employee.h
+++ employee.h
@@ -6,12 +6,6 @@ typedef struct {
     char *name;
     char *phone;
     double salary;
-<<<<<<< HEAD
 } Employee, *PtrToEmployee;
 
 typedef const Employee *PtrToConstEmployee;
-=======
-} Employee, *PtrToEmployee
-
-typedef const Employee *PtrToEmployee;
->>>>>>> d25131cf7c6714ba270337fc4edd2b7ee4a70306
